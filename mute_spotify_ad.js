d293f9ad5eb74663b4e32e614a261b9758aa38c3
kineforce <lucas-sm2010@hotmail.com>
2021-07-22 18:37:08 -0300

    Spotify Mute AD's v0.1

---
+++ mute_spotify_ad.js
@@ -0,0 +1,53 @@
+// Control variables
+var mute = 'off';
+var play = 'on';
+
+
+main_listener = setInterval(()=>{
+	// Sibling element of the element who shows the duration of the sound
+	let progress_bar = document.getElementsByClassName('progress-bar-wrapper')[0];
+
+	// Element who shows the duration of the sound
+	let duration_sound = progress_bar.nextElementSibling.textContent;
+	
+	
+	// If the sound duration is equal to 0:30, then it's a spotify ad
+    if (duration_sound === "0:30"){
+		// Log
+		console.log("----> Keeping the sound muted, it's a Spotify AD!")
+
+		// Flag to make only one click when the spotify ad comes
+		play = 'off';
+
+		// Then mutes the sound 
+		if (mute == 'off'){
+			// Click on the mute button 
+			btn = document.getElementsByClassName('volume-bar__icon-button control-button')[0].click();
+
+			// Change mute variable value to avoid multiple clicks
+			mute = 'on';
+		}
+
+	} 
+
+	// If the sound duration is not equal to 0:30, then it's just a regular song
+	if (duration_sound !== "0:30"){
+		// Log
+		console.log("----> Playing the sound, isn't a Spotify AD!")
+		
+		// Flag to keep the flow when it'll need to mute the sound
+		mute = 'off';
+
+		// Unmute the sound again
+		if (play == 'off'){
+			// Click on the mute button
+			btn = document.getElementsByClassName('volume-bar__icon-button control-button')[0].click();
+
+			// Keep playing the sound, no need to click again to mute if isn't an spotify ad
+			play = 'on';
+		}
+
+
+	}	
+
+}, 500)
