62b50a817dd045632fcda770b3408d592a3653b4
kineforce <lucas-sm2010@hotmail.com>
2021-07-26 21:59:55 -0300

    refresh button status each change

--- mute_spotify_ad_observer.js
+++ mute_spotify_ad_observer.js
@@ -1,27 +1,30 @@
 let info_curr_song = document.querySelectorAll('[data-testid="now-playing-widget"]')[0];
 let btn = document.getElementsByClassName('volume-bar__icon-button control-button')[0];
-let is_muted = btn.getAttribute('aria-label');
 
 const root_playing_observer = new MutationObserver(function(mutated_elements) {
     
+    let is_muted = btn.getAttribute('aria-label');
+
     mutated_elements.forEach((element) => {
         let aria_label = element.target.ariaLabel;
 
         if (aria_label == "Advertisement"){
             console.log("----> Keeping the sound muted, it's a Spotify AD!")
-            console.log(mutated_elements)
 
             if (is_muted == 'Mute'){
                 btn.click();
             }
 
+
         } else {
             console.log("----> Playing the sound, isn't a Spotify AD!")
-            console.log(mutated_elements)
 
             if (is_muted == 'Unmute'){
                 btn.click();
             }
+
+            console.log(is_muted)
+
         }
 
     })
